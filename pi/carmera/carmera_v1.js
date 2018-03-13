30a87798c569f32275eec5db9bd99b0cef37f019
duckheewon <fain9301@yahoo.com>
2018-03-13 18:17:47 +0900

    testing m3 folder

--- pi/carmera/carmera_v1.js
+++ pi/carmera/carmera_v1.js
@@ -1,74 +1,96 @@
-var RaspiCam = require('raspicam'); //carmera module
-var dl = require('delivery'); //send image module
-var moment = require('moment'); //time module
-var socket = require('socket.io-client')(''); //ip address 
+'use strict';
 
 
-var option = {
-    width: 600,
-    height: 420,
-    mode: 'timelapse',
-    awb: 'off',
-    encoding: 'jpg',
-    output: "./images/image_%03d.jpg", // image_000001.jpg, image_000002.jpg,... moment().format('YYYYMMDDHHmmss') + ".jpg"
-    q: 50,
-    timeout: 0, // take a total of 4 pictures over 12 seconds , 0 일경우 무제한 촬영
-    timelapse: 1000 * 15, //1시간 단위로 촬영
-    nopreview: true,
-    th: '0:0:0'
-};
+var moment = require('moment'); // moment 시간 모듈
+var exec_photo = require('child_process').exec;
+var fs = require('fs');
 
-var camera = new RaspiCam(option);
+module.exports = function(pool, socket, delivery) { //함수로 만들어 객체 app을 전달받음    
+    return {
+        init: function() {
 
-//소켓통신으로 이미지 파일을 서버로 전송
-socket.on('connect', function() {
-    console.log("Sockets connected");
-    //delivery 패키지 이용
-    delivery = dl.listen(socket);
-    delivery.connect();
 
-    delivery.on('delivery.connect', function(delivery) {
+            var current_min = moment().format('m'); // 현재 시간 분 설정
+            var shooting_time = 60; //사진 촬영 인터벌
+            var sub_min = 0; //정각에서 남은 시간
 
-        delivery.on('send.success', function(file) {
-            console.log('File sent successfully!');
-        });
-    });
+            //인터벌 함수 실행
+            if (current_min == 0) { // 만약 0이면 바로 촬영 시작
+                sub_min = 0;
+            } else { // 0이 아닐시 남은 시간 설정 후 촬영 시작
+                sub_min = shooting_time - current_min;
+            }
 
-});
 
-// 카메라 설정 시간 간격 마다 촬영 실행
-function camera_starting() {
-    camera_setting(); // 처음 한번 촬영
-    camera_interval = setInterval(camera_setting, 1000 * 60 * shooting_time); // 설정 시간 후에 반복 촬영
-};
 
-// 현재 시간으로 카메라 설정 세팅
-function camera_setting() {
-    timeInMs = moment().format('YYYYMMDDHHmmss');
-    photo_path = __dirname + "/images/" + timeInMs + ".jpg";
-    cmd_photo = 'raspistill -vf -t 1 -w 600 -h 420 -o ' + photo_path;
-    setTimeout(() => {
-        camera_shooting();
-    }, 500);
-};
+            console.log('sub_min : ' + sub_min);
 
-// 설정된 값으로 카메라 촬영
-function camera_shooting() {
-    exec_photo(cmd_photo, function(err, stdout, stderr) {
-        if (err) {
-            console.log('child process exited with shooting_photo error code', err.code);
-            return;
+            //사진한번 찍고
+            this.camera_shooting();
+
+            setTimeout(() => {
+                console.log('timeout ' + sub_min + ' minute');
+                setInterval(this.camera_shooting, 1000 * 60 * shooting_time); // 설정 시간 후에 반복 촬영
+            }, 1000 * 60 * sub_min); // 제한된 시간 후에 촬영 시작          
+        },
+        // 사진 촬영 
+        camera_shooting: function() {
+            var timeInMs = moment().format('YYYYMMDDHHmmss');
+            var dir_name = moment().format('YYYYMM');
+            var photo_path = process.cwd() + "/images/" + dir_name + "/" + timeInMs + ".jpg";
+            var cmd_photo = 'raspistill -vf -hf -ex auto -ev 0 -awb auto -w 1640 -h 922 -o ' + photo_path;
+
+            //폴더 생성
+            if (!fs.existsSync(process.cwd() + '/images')) {
+                fs.mkdirSync(process.cwd() + '/images', '0777');
+            }
+
+            if (!fs.existsSync(process.cwd() + '/images/' + dir_name)) {
+                fs.mkdirSync(process.cwd() + '/images/' + dir_name, '0777');
+            }
+
+            exec_photo(cmd_photo, function(err, stdout, stderr) {
+
+                if (err) {
+                    console.log('child process exited with shooting_photo error code', err.code);
+                    return;
+                }
+
+                console.log("photo captured with filename: " + timeInMs);
+                pool.getConnection(function(err, connection) {
+                    console.log("camera connetion");
+
+                    // 마지막으로 연결된 센서 정보 가져오기
+                    connection.query(' select * from seosan_settings  order by createdAt desc limit 0,1 ', function(err, result, fields) {
+                        if (err) console.log(err);
+
+                        console.log(result);
+
+                        if (result.length != 0 && result[0].st_serial) {
+
+                            var stats = fs.statSync(process.cwd() + '/images/' + dir_name + "/" + timeInMs + ".jpg");
+
+                            //정보 insert
+                            connection.query(' insert into seosan_images  (si_serial, si_path, si_filename, si_filesize, createdAt, updatedAt) values (?, ?, ?, ?, NOW(), NOW())', [result[0].st_serial, dir_name, timeInMs + ".jpg", stats.size], function(error, result) {
+                                if (!error) {
+                                    console.log(result);
+                                }
+                            });
+
+                            // 촬영 이미지 전송
+                            delivery.send({
+                                name: timeInMs,
+                                path: process.cwd() + '/images/' + dir_name + "/" + timeInMs + ".jpg",
+                                params: { serial: result[0].st_serial, filename: timeInMs + ".jpg", path: dir_name, filesize: stats.size }
+                            });
+                        }
+
+                        connection.release();
+
+                    });
+                });
+
+            });
         }
-        console.log("photo captured with filename: " + timeInMs);
-        camera_sending();
-    });
-}
-
-// 촬영 이미지 전송
-function camera_sending() {
-    delivery.send({
-        name: timeInMs,
-        path: __dirname + '/images/' + timeInMs + ".jpg",
-        params: { channel: field_id, img_name: timeInMs + ".jpg" }
-    });
+    }
 };
