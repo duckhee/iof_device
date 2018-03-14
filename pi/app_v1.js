cf5f6944e2a5fc2632ddb6be2618189ee7ae3228
duckhee <fain9301@yahoo.com>
2018-03-14 10:12:41 +0900

    testing

---
+++ pi/app_v1.js
@@ -0,0 +1,10 @@
+//default config
+var config = require('./config.json');
+
+//var field_id = config.channel; // field_id 값 설정
+//var water_stop_time = config.water_stop_time; // water_stop_time 값 설정
+//var shooting_time = config.shooting_time; //shooting time 값 설정
+
+var current_min;
+var sub_min;
+var camera_interval;
