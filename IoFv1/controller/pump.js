4dd478025357d1f1cc525bedba2c1422c84366d1
duckhee <fain9301@yahoo.com>
2018-04-08 01:05:12 +0900

    testing control motor

--- IoFv1/controller/pump.js
+++ IoFv1/controller/pump.js
@@ -1,1 +1,14 @@
 var onoff = require('onoff');
+const gpio = onoff.Gpio;
+
+const switch_pump = new gpio(17, 'out');
+
+exports.control_motor = function(control_value) {
+    if (control_value === 0) {
+        console.log('motor turn off');
+    } else if (control_value === 1) {
+        console.log('motor turn on');
+    } else {
+        console.log('motor turn error');
+    }
+}
