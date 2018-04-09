80338d3676f7ca612c7df877054bc86016f7aa55
duckhee <fain9301@yahoo.com>
2018-04-09 17:47:47 +0900

    git add

--- IoFv1/routes/arduino.js
+++ IoFv1/routes/arduino.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const arduinoController = require('../controller/arudino');
-
+var moment = require('moment');
 module.exports = function(pool, socket){
+    
+    var fisrt_chk = 0;
+
+    var data = arduinoController.sensor_mesurement();
+    console.log('get mesurement data ::::::::::: ', data);
+    var re = /[^\,^\-^A-Z^\d(.\d+)^\s]/gi;
+    var datfilter = data.replace(re, '');
+    console.log(dataJoin);
+    var sensorObj = datfilter.split(',');
+    var sensorData = [];
+    var firstData = sensorObj[0];
+    var secondData = sensorObj[1];
+    var datajoin;
+    
 
 }
