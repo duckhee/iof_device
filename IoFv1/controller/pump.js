4bbf5ec204d367ba7cee97977b788e27f0f069d6
duckhee <fain9301@yahoo.com>
2018-04-09 17:52:44 +0900

    tesitng

--- IoFv1/controller/pump.js
+++ IoFv1/controller/pump.js
@@ -6,10 +6,13 @@ const switch_pump = new gpio(17, 'out');
 exports.control_motor = function(control_value) {
     if (control_value === 0) {
         console.log('motor turn off');
+        switch_pump.writeSync(0);
     } else if (control_value === 1) {
         console.log('motor turn on');
+        switch_pump.writeSync(1);
     } else {
         console.log('motor turn error');
+        switch_pump.writeSync(0);
     }
 }
 
