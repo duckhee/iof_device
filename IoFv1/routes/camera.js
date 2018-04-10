f8611a8f2b498ae3a7834a619c2fb1ee9e2a30e5
duckheewon <fain9301@yahoo.com>
2018-04-10 16:28:58 +0900

    testing

--- IoFv1/routes/camera.js
+++ IoFv1/routes/camera.js
@@ -53,6 +53,8 @@ module.exports = function(pool, socket, delivery) { //함수로 만들어 객체
 
                 if (err) {
                     console.log('child process exited with shooting_photo error code', err.code);
+                    console.log('child process exited with shooting_photo error code :::::::::::::::: ', err);
+                    console.log('child process exited with shooting_photo error stack ::::::::: ', err.stack);
                     return;
                 }
 
