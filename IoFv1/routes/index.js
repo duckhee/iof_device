ad9f3dcdb3285d7abe68a0a19049c8aa7400e05b
duckheewon <fain9301@yahoo.com>
2018-04-10 16:07:31 +0900

    fix

--- IoFv1/routes/index.js
+++ IoFv1/routes/index.js
@@ -1,63 +1,60 @@
 'use strict';
-module.exports = function (pool) {//함수로 만들어 객체 app을 전달받음
+module.exports = function(pool) { //함수로 만들어 객체 app을 전달받음
     var express = require('express');
     var router = express.Router();
 
-    router.get('/', function (req, res) {
-        pool.getConnection(function (err, connection)
-        {
+    router.get('/', function(req, res) {
+        pool.getConnection(function(err, connection) {
             // Use the connection
-            var stmt = 'SELECT * from seosan_settings';
-            connection.query(stmt, function (err, rows) {
+            var stmt = 'SELECT * from iof_settings';
+            connection.query(stmt, function(err, rows) {
                 if (err) console.error("err : " + err);
                 console.log("rows : " + JSON.stringify(rows));
-                connection.release();  
-                res.render('index', { title: 'Devices' , devices: rows});  
+                connection.release();
+                res.render('index', { title: 'Devices', devices: rows });
                 // Don't use the connection here, it has been returned to the pool.
             });
         });
     });
 
-    router.get('/dashboard/:serial', function (req, res) {
-        pool.getConnection(function (err, connection)
-        {
+    router.get('/dashboard/:serial', function(req, res) {
+        pool.getConnection(function(err, connection) {
             // Use the connection
             var stmt = ' SELECT * from seosan_data where sd_serial = ? order by createdAt desc limit 0,3 ';
-            connection.query(stmt, [req.params.serial], function (err, rows) {
+            connection.query(stmt, [req.params.serial], function(err, rows) {
                 if (err) console.error("err : " + err);
                 var data_list = rows;
 
-                var stmt = ' SELECT * from seosan_images where si_serial = ? order by createdAt desc limit 0,1 ';
-                connection.query(stmt, [req.params.serial], function (err, row) {
+                var stmt = ' SELECT * from iof_images where si_serial = ? order by createdAt desc limit 0,1 ';
+                connection.query(stmt, [req.params.serial], function(err, row) {
                     if (err) console.error("err : " + err);
-                    
-                    connection.release();  
-                    res.render('view', { title: 'Dashboard' , devices: data_list, img_path: row[0].si_path + "/" + row[0].si_filename });  
+
+                    connection.release();
+                    res.render('view', { title: 'Dashboard', devices: data_list, img_path: row[0].si_path + "/" + row[0].si_filename });
                     // Don't use the connection here, it has been returned to the pool.
                 });
-                
+
             });
         });
     });
 
-    router.get('/table/:serial/:page', function (req, res) {
-        pool.getConnection(function (err, connection)
-        {
+    router.get('/table/:serial/:page', function(req, res) {
+        pool.getConnection(function(err, connection) {
             // Use the connection
             var stmt = ' SELECT * from seosan_data where sd_serial = ? order by createdAt desc limit ?,10 ';
-            connection.query(stmt, [req.params.serial, (req.params.page-1)*10], function (err, rows) {
+            connection.query(stmt, [req.params.serial, (req.params.page - 1) * 10], function(err, rows) {
                 if (err) console.error("err : " + err);
                 console.log("rows : " + JSON.stringify(rows));
-                connection.release();  
-                res.render('view', { title: 'Dashboard' , devices: rows});  
+                connection.release();
+                res.render('view', { title: 'Dashboard', devices: rows });
                 // Don't use the connection here, it has been returned to the pool.
             });
         });
     });
 
-    router.post('/setting', function (req, res) {
+    router.post('/setting', function(req, res) {
         res.render('index', { title: 'Devices' });
     });
 
-    return router;	//라우터를 리턴
+    return router; //라우터를 리턴
 };
