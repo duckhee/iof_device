6a710ca44d15fd6af55fc502b75c089a572608df
duckheewon <fain9301@yahoo.com>
2018-04-10 17:57:02 +0900

    fix filepath

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -1,19 +1,13 @@
 var arudinoController = require('./controller/arudino');
 
-arudinoController.sensor_info(function(err, result) {
-    if (err) {
-        console.log('arudino get info error :::::::::: ', err);
-        process.exit();
-    } else {
-        console.log('sensor info :::::: ', result);
-    }
+arudinoController.sensor_info(function(result) {
+
+
+    console.log('sensor info :::::: ', result);
 });
 
-arudinoController.sensor_mesurement(function(err, result) {
-    if (err) {
-        console.log('arduino get mesurement error ::::::: ', err);
-        process.exit();
-    } else {
-        console.log('get value ::::::::: ', result);
-    }
+arudinoController.sensor_mesurement(function(result) {
+
+    console.log('get value ::::::::: ', result);
+
 });
