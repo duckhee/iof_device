4ae1f792f09a143731eb08c9d060a7569026f26d
duckheewon <fain9301@yahoo.com>
2018-04-11 14:22:23 +0900

    fix

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -7,34 +7,42 @@ arudinoController.sensor_info(function(err, result) {
     if (err) {
         console.log('info error :::::: ', err);
         process.exit();
-    } else {
+    } else if (result) {
         console.log('sensor info :::::::::: ', result);
         arudinoController.clear_sensor(function(err, result2) {
             if (err) {
                 console.log('clear data error ::::::: ', err);
                 process.exit();
-            } else {
+            } else if (result2) {
                 console.log('clear data :::::::: ', result2);
 
                 arudinoController.sensor_mesurement(function(err, result3) {
                     if (err) {
                         console.log('get data error :::::::: ', err);
                         process.exit();
-                    } else {
+                    } else if (result3) {
                         console.log('get data :::::::: ', result3);
                         arudinoController.clear_sensor(function(err, result5) {
                             if (err) {
                                 console.log('clear data error ::::::: ', err);
                                 process.exit();
-                            } else {
+                            } else if (result5) {
                                 console.log('clear data :::::::: ', result5);
                                 process.exit();
+                            } else {
+                                console.log('null');
                             }
 
                         });
+                    } else {
+                        console.log('null');
                     }
                 });
+            } else {
+                console.log('null');
             }
         });
+    } else {
+        console.log('null');
     }
 });
