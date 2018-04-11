69d80309f171cbdcc3e351df07a569308d614e99
duckheewon <fain9301@yahoo.com>
2018-04-11 11:28:38 +0900

    testing

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -6,10 +6,13 @@ arudinoController.sensor_mesurement(function(err, result2) {
         console.log('sensor value callback :::::::::: ', result2);
     }
 });
-process.nextTick(arudinoController.sensor_info(function(err, result) {
-    if (result) {
-        console.log('sensor info callback :::::::::::', result);
 
+function test() {
+    arudinoController.sensor_info(function(err, result) {
+        if (result) {
+            console.log('sensor info callback :::::::::::', result);
 
-    }
-}));
+        }
+    });
+}
+process.nextTick(test);
