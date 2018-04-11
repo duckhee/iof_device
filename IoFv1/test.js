0d455a857e5db7fe6e129c0c0849e0b143c47577
duckheewon <fain9301@yahoo.com>
2018-04-11 14:38:03 +0900

    fix

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -9,39 +9,19 @@ arudinoController.sensor_info(function(err, result) {
         process.exit();
     } else if (result) {
         console.log('sensor info :::::::::: ', result);
-        arudinoController.clear_sensor(function(err, result2) {
+        arudinoController.sensor_mesurement(function(err, result3) {
             if (err) {
-                console.log('clear data error ::::::: ', err);
+                console.log('get data error :::::::: ', err);
                 process.exit();
-            } else if (result2) {
-                console.log('clear data first :::::::: ', result2);
+            } else if (result3) {
+                console.log('get data :::::::: ', result3);
+                process.exit();
 
-                arudinoController.sensor_mesurement(function(err, result3) {
-                    if (err) {
-                        console.log('get data error :::::::: ', err);
-                        process.exit();
-                    } else if (result3) {
-                        console.log('get data :::::::: ', result3);
-                        arudinoController.clear_sensor(function(err, result5) {
-                            if (err) {
-                                console.log('clear data error ::::::: ', err);
-                                process.exit();
-                            } else if (result5) {
-                                console.log('clear data last :::::::: ', result5);
-                                process.exit();
-                            } else {
-                                console.log('null clear data last');
-                            }
-
-                        });
-                    } else {
-                        console.log('null sesnor meurement ');
-                    }
-                });
             } else {
-                console.log('null clear data first');
+                console.log('null sesnor meurement ');
             }
         });
+
     } else {
         console.log('null sensor info ');
     }
