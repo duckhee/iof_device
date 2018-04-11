ae2eca50961c55820441f6fe8abb827d30362895
duckheewon <fain9301@yahoo.com>
2018-04-11 15:02:24 +0900

    fix

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -3,14 +3,14 @@ var arudinoController = require('./controller/arudino');
 var flag = false;
 
 
-arudinoController.sensor_mesurement(function(err, result) {
+arudinoController.sensor_info(function(err, result) {
     if (err) {
         console.log('info error :::::: ', err);
         process.exit();
     } else if (result) {
         console.log('sensor info :::::::::: ', result);
 
-        arudinoController.sensor_info(function(err, result3) {
+        arudinoController.sensor_mesurement(function(err, result3) {
             if (err) {
                 console.log('get data error :::::::: ', err);
                 process.exit();
