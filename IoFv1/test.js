729d969ae87063c30501b29c16fc751b77714bea
duckheewon <fain9301@yahoo.com>
2018-04-11 13:53:56 +0900

    fix

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -2,12 +2,11 @@ var arudinoController = require('./controller/arudino');
 
 var flag = false;
 
-if (flag === false) {
-    arudinoController.sensor_info();
-    arudinoController.clear_data();
-    flag = true;
-}
 
-while (1) {
-    arudinoController.sensor_mesurement();
-}
+arudinoController.sensor_info();
+arudinoController.clear_data();
+
+
+
+
+arudinoController.sensor_mesurement();
