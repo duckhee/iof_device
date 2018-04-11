74758a18587a570149e57bdb5a8638bd41bf6ac7
duckheewon <fain9301@yahoo.com>
2018-04-11 11:22:45 +0900

    testing

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -3,7 +3,7 @@ var arudinoController = require('./controller/arudino');
 arudinoController.sensor_info(function(err, result) {
     if (result) {
         console.log('sensor info callback :::::::::::', result);
-        process.nextTick();
+
         arudinoController.sensor_mesurement(function(err, result2) {
             if (result2) {
                 console.log('sensor value callback :::::::::: ', result2);
