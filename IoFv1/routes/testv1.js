12af72ebfb7bfd958827ba9a444e66d17ab74583
duckheewon <fain9301@yahoo.com>
2018-04-11 13:36:59 +0900

    fix

---
+++ IoFv1/routes/testv1.js
@@ -0,0 +1,9 @@
+'use strict';
+//수분 측정 모듈//
+var moment = require('moment'); // moment 시간 모듈
+var SerialPort = require('serialport'); //아두이노와 시리얼 통신할 수 있는 모듈
+var fs = require('fs');
+
+module.exports = function(pool, socket) {
+
+}
--- IoFv1/test.js
+++ IoFv1/test.js
@@ -1,26 +1,13 @@
 var arudinoController = require('./controller/arudino');
 
+var flag = false;
 
-process.nextTick(test1);
-
-process.nextTick(test);
-
-
-function test() {
-    arudinoController.sensor_info(function(err, result) {
-        if (result) {
-            console.log('sensor info callback :::::::::::', result);
-
-        }
-    });
+if (flag === false) {
+    arudinoController.sensor_info();
+    arudinoController.clear_data();
+    flag = true;
 }
 
-function test1() {
-
-    arudinoController.sensor_mesurement(function(err, result2) {
-        if (result2) {
-            console.log('sensor value callback :::::::::: ', result2);
-        }
-    });
-
+while (1) {
+    arudinoController.sensor_mesurement();
 }
