fe32a66e89f85763d4e0dbb9e7bf9ab1e5cc5a80
duckheewon <fain9301@yahoo.com>
2018-04-12 17:04:46 +0900

    tesitng socket send delay

--- IoFv1/routes/testv1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-//수분 측정 모듈//
-var moment = require('moment'); // moment 시간 모듈
-var SerialPort = require('serialport'); //아두이노와 시리얼 통신할 수 있는 모듈
-var fs = require('fs');
-
-module.exports = function(pool, socket) {
-
-}
