f20b9326494f071a0b3f2ce696015ea1c13b5bff
duckheewon <fain9301@yahoo.com>
2018-04-13 17:41:55 +0900

    add table query

--- IoFv1/routes/testarudinorouterv2.js
+++ IoFv1/routes/testarudinorouterv2.js
@@ -94,8 +94,7 @@ module.exports = function(pool, socket, serialNum, defualtsensingtime) {
                                 } else {
                                     console.log('insert data pi result ::::: ', result);
                                     socket.emit('sensor_data_request', sensorSoil);
-                                    port.flush();
-                                    port.write('d');
+
                                     /*
                                         console.log('get setting sensing time :::: ', defualtsensingtime);
                                         var current_min = moment().format('m'); // 현재 시간 분 설정
@@ -120,6 +119,8 @@ module.exports = function(pool, socket, serialNum, defualtsensingtime) {
                                         }, 1000 * 60 * sub_min); // 제한된 시간 후에 센싱 시작
                     */
                                     conn.release();
+                                    port.flush();
+                                    port.write('d');
                                 }
                             });
                         }
