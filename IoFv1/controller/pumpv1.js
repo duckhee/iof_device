15dc63219bc3a1b37c0410618e691b24abd46d9a
duckheewon <fain9301@yahoo.com>
2018-04-18 13:59:29 +0900

    tesitng pump

---
+++ IoFv1/controller/pumpv1.js
@@ -0,0 +1,60 @@
+var pigpio = require('pigpio');
+var util = require('../util/util');
+var gpio = pigpio.Gpio;
+const switch_pump = new gpio(17, {
+    mode: gpio.OUTPUT,
+    //  alert: true
+});
+switch_pump.setDirection('out');
+module.exports = function(StatusFlag, TurnFlag, delayTime) {
+    var autoFlag = true;
+    this.autoFlag = StatusFlag;
+    var TurningFlag = false;
+    this.TurningFlag = TurnFlag;
+    var waitTime = 3;
+    this.waitTime = delayTime;
+
+    return {
+        init: function() {
+            console.log('Hardware Revision: ' + pigpio.hardwareRevision().toString(16));
+            console.log('play moter status :: ', StatusFlag);
+            console.log('play moter autoFlag status :: ', autoFlag);
+            console.log('Turn on off status :: ', TurnFlag);
+            console.log('TurningFlag status :: ', TurningFlag);
+            console.log('delay time :: ', delayTime);
+            console.log('wait time :::: ', waitTime);
+            console.log('get type (input or output) : ', switch_pump.direction());
+        },
+        auto: function(delayTime) {
+            console.log('start auto');
+            console.log('delayTime ::: ', delayTime);
+            if (!util.isEmpty(delayTime)) {
+                //delay time not null
+            } else {
+                delayTime = 3;
+            }
+        },
+        Toggle: function() {
+            console.log('toggle water switch : ', switch_pump.digitalRead());
+            switch_pump.digitalWrite(switch_pump.digitalRead() ^ 1);
+            console.log('toggle comfirm water switch : ', switch_pump.digitalRead());
+        },
+        manual: function(TurnStatus) {
+            console.log('start not auto');
+            if (TurnStatus === 0) {
+                switch_pump.digitalWrite(0);
+                console.log('switch pump value : ', switch_pump.digitalRead());
+            } else if (TurnStatus === 1) {
+                switch_pump.digitalWrite(1);
+                console.log('switch pump value : ', switch_pump.direction());
+            } else {
+                console.log('Not value !');
+            }
+        },
+        TimeOut: function() {
+            console.log('time off');
+            switch_pump.digitalWrite(0);
+        }
+    }
+
+}
--- IoFv1/test.js
+++ IoFv1/test.js
@@ -1,29 +1,5 @@
-var arudinoController = require('./controller/arudino');
-//var arudinoController = require('./controller/arduinov1');
+var pump = require('./controller/pumpv1');
 
-var flag = false;
-
-
-arudinoController.sensor_info(function(err, result) {
-    if (err) {
-        console.log('info error :::::: ', err);
-        process.exit();
-    } else if (result) {
-        console.log('sensor info :::::::::: ', result);
-        arudinoController.sensor_mesurement(function(err, result3) {
-            if (err) {
-                console.log('get data error :::::::: ', err);
-                process.exit();
-            } else if (result3) {
-                console.log('get data :::::::: ', result3);
-                process.exit();
-
-            } else {
-                console.log('null sesnor meurement ');
-            }
-        });
-
-    } else {
-        console.log('null sensor info ');
-    }
-});
+pump.init();
+console.log('end ');
+process.exit();
