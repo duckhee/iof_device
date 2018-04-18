71ddda0ee619deecc96e57cb4fd01ab3841da9ba
duckheewon <fain9301@yahoo.com>
2018-04-18 17:04:49 +0900

    testing

--- IoFv1/test.js
+++ IoFv1/test.js
@@ -1,6 +1,8 @@
 var pump = require('./controller/pumpv2');
 var StatusFlag = false,
     TurnFlag = true,
-    delayTime = 5;
+    delayTime = 5,
+    dataValue = 10,
+    turnValue = 50;
 pump(StatusFlag, TurnFlag, delayTime, dataValue, turnValue).init();
 console.log('end ');
