3d18997bbe0d6a82915eea5fcf30a345f4e583bf
duckhee <fain9301@yahoo.com>
2018-04-20 15:40:00 +0900

    start socket setting

--- IoFv1/routes/setting.js
+++ IoFv1/routes/setting.js
@@ -15,6 +15,28 @@ module.exports = function(socket, serialNum) {
         var sendSerial = { "serial": serial }
         socket.emit('device_setting_first', { sendSerial });
     }
-
+    socket.on('device_setting_check_' + serial, (data) => {
+        if (data.check === 1) {
+            console.log('checking this device : ', data);
+            var SettingInfo = {
+                "st_serial": data.serial,
+                "st_shootingtime": data.st_shootingtime,
+                "st_watertime": data.st_watertime
+            };
+            SettingController.InsertSetting(SettingInfo, function(err, result) {
+                if (err) {
+                    console.log('setting socket insert error :::: ', err);
+                } else {
+                    if (!util.isEmpty(result)) {
+                        console.log('setting value : ', result);
+                    } else {
+                        console.log('setting null ');
+                    }
+                }
+            });
+        } else {
+            console.log('not checking this device  :::: ', data);
+        }
+    });
 
 }
