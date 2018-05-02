d08b44b24737fb08556f70547328cd62f98ad149
duckhee <fain9301@yahoo.com>
2018-05-02 16:32:41 +0900

    testing

--- IoFv2/socket/sendsensor.js
+++ IoFv2/socket/sendsensor.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var moment = require('moment');
-var fs = require('fs');
-var serialPort = require('serialport');
-var util = require('../util/util');
-var flag = false;
+var SensorController = require('../controller/sensingcontroller');
+
+module.exports = function(socket) {
+
+}
