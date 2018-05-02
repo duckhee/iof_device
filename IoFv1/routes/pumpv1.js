8496c732817c6a23d90475029023e6b0603035c5
duckhee <fain9301@yahoo.com>
2018-05-02 16:30:56 +0900

    testing socket

--- IoFv1/routes/pumpv1.js
+++ IoFv1/routes/pumpv1.js
@@ -1,1 +1,4 @@
 var PumpController = require('../controller/pumpv2');
+
+var express = require('express');
+var router = express.Router();
---
+++ IoFv2/socket/sendsensor.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var moment = require('moment');
+var fs = require('fs');
+var serialPort = require('serialport');
+var util = require('../util/util');
+var flag = false;
