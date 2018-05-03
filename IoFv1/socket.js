1891f0a1d5390086dc9055f5a6665940a136b540
duckhee <fain9301@yahoo.com>
2018-05-03 17:19:59 +0900

    tesitng

--- IoFv1/socket.js
+++ IoFv1/socket.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var CameraController = require('./router/camera');
+var camera = require('./routes/camera');
 
 module.exports = function(socket, delivery, serialNum, cameraTime) {
     socket.on('shooting_' + serialNum, function(data) {
         console.log('socket testing  value :::', data);
         console.log('socket command :::: ', data.cmd);
         if (data.cmd === '1') {
-            CameraController(socket, delivery, serialNum, cameraTime).camera_shooting();
+            camera(socket, delivery, serialNum, cameraTime).camera_shooting();
         } else {
             console.log('not command shooting :::', data.cmd);
         }
